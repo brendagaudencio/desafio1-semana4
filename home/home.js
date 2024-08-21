8cc75a88dcfbc473ba4d1db6d2dedec4bf5deeaf
brendagaudencio <brenda.gaudencio@outlook.com>
2024-08-21 02:36:18 -0300

    feat: responsividade da home, navbar e footer

--- home/home.js
+++ home/home.js
@@ -34,3 +34,14 @@ formNewsletter.addEventListener("submit", (e) => {
 
   let emailKeyHome = localStorage.setItem("emailKeyHome", emailHome.value);
 });
+
+function menuShow() {
+  let menuMobile = document.querySelector(".mobile-menu");
+  if (menuMobile.classList.contains("open")) {
+    menuMobile.classList.remove("open");
+    document.querySelector(".icon").src = "../assets/menu_white_36dp.svg";
+  } else {
+    menuMobile.classList.add("open");
+    document.querySelector(".icon").src = "../assets/close_white_36dp.svg";
+  }
+}
